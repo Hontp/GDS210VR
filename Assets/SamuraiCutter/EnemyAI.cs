41c8a97753dcf2732688a170ca66ca03a8c8676b
Krystal McDonald <krystalmc478@gmail.com>
2019-11-12 20:27:07 +0800

    Added NavigationMesh(we need a better mesh), NavAgents and working on Round spawning

--- Assets/SamuraiCutter/EnemyAI.cs
+++ Assets/SamuraiCutter/EnemyAI.cs
@@ -1,83 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using SamuraiCutter;
 
-public class EnemyAI : MonoBehaviour
+namespace SamuraiCutter
 {
-    public Transform target;
-    private Transform myTransform;
 
-    public float rotationSpeed = 3,
-                 MoveSpeed = 4,
-                 MinDist = 5,
-                 MaxDist = 10f;
-
-    private void Awake()
+    public class EnemyAI : MonoBehaviour
     {
-        if (!target)
+        public Transform target;
+        private Transform myTransform;
+        private NavMeshAgent nma;
+        public float rotationSpeed = 3,
+                    MoveSpeed = 4,
+                    MinDist = 5,
+                    MaxDist = 10f;
+
+        private SpawnEnemy spawnEnemy;
+        private void Start()
         {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
+            nma = GetComponent<NavMeshAgent>();
+            nma.speed = MoveSpeed;
+            nma.SetDestination(GameManager._instance.playerPos.position);
+            spawnEnemy = GameManager._instance.spawnEnemy;
         }
 
-        myTransform = transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Movement();
-    }
-
-    public void Movement()
-    {
-
-        #region SimpleMethod
-        float distance = Vector3.Distance(transform.position, target.position);
-
-        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-
-        if (distance >= MinDist)
+        // Update is called once per frame
+        void Update()
         {
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
-            if (distance <= MaxDist)
+            //Movement();
+        }
+
+        public void Movement()
+        {
+
+            #region SimpleMethod
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+            if (distance >= MinDist)
             {
-                //run attack trigger here
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+                if (distance <= MaxDist)
+                {
+                    //run attack trigger here
+                }
             }
+            Debug.Log(distance);
+            #endregion
+
+
+            #region Testing
+
+            #region Simple No If Statments
+            ///* Rotation - Look At Player */
+            //myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+            ///* Movement - Move Towards Player */
+            //myTransform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            #endregion
+
+            #region Issues RigidBody
+            /* Rigidbody component will need to be referenced */
+
+            //float distance = Vector3.Distance(transform.position, target.position);
+
+            //if (distance >= MinDist)
+            //{
+            //    myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+            //    rb.AddForce(myTransform.forward * MoveSpeed * Time.deltaTime);
+
+            //    if (distance < MinDist)
+            //    {
+            //        //attack                
+            //    }
+            //}
+            #endregion
+
+            #endregion
+
         }
-        Debug.Log(distance);
-        #endregion
-
-
-        #region Testing
-
-        #region Simple No If Statments
-        ///* Rotation - Look At Player */
-        //myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-
-        ///* Movement - Move Towards Player */
-        //myTransform.position += transform.forward * MoveSpeed * Time.deltaTime;
-        #endregion
-
-        #region Issues RigidBody
-        /* Rigidbody component will need to be referenced */
-
-        //float distance = Vector3.Distance(transform.position, target.position);
-
-        //if (distance >= MinDist)
-        //{
-        //    myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-
-        //    rb.AddForce(myTransform.forward * MoveSpeed * Time.deltaTime);
-
-        //    if (distance < MinDist)
-        //    {
-        //        //attack                
-        //    }
-        //}
-        #endregion
-
-        #endregion
-
     }
+
 }
--- Assets/SamuraiCutter/GettingHit.cs
+++ Assets/SamuraiCutter/GettingHit.cs
@@ -2,64 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GettingHit : MonoBehaviour
+namespace SamuraiCutter
 {
-    public GameObject brokenMesh, replacedMesh;
-    public SpawnEnemy spawnEngine;
-    public bool bypass;
-    public float explosionRadius, explosionPower;
-    public Vector3 explosionPos;
 
-    private void Awake()
+    public class GettingHit : MonoBehaviour
     {
-        spawnEngine = GameObject.Find("EnemySpawnManager").GetComponent<SpawnEnemy>();
-    }
+        public GameObject brokenMesh, replacedMesh;
+        public SpawnEnemy spawnEngine;
+        public bool bypass;
+        public float explosionRadius, explosionPower;
+        public Vector3 explosionPos;
+        public UnityEngine.AI.NavMeshAgent nma;
 
-    private void Update()
-    {
-        if(bypass)
+        private void Awake()
         {
-            bypass = false;
-            Hit();
-            Invoke("DestroyMe", 2f);
+            spawnEngine = GameObject.Find("EnemySpawnManager").GetComponent<SpawnEnemy>();
+            
         }
-    }
-    void OnTriggerEnter(Collider hittingCollider)
-    {
-        Debug.Log(hittingCollider.transform.tag);
-        if(hittingCollider.CompareTag("PlayerSword"))
-        {
-            Hit();
-            explosionPos = hittingCollider.transform.position;
-            Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
-            foreach (Collider hit in colliders)
-            {
-                if (hit.CompareTag("EnemyChunk"))
-                {
-                    Rigidbody rb = hit.GetComponent<Rigidbody>();
-                    if (rb != null)
-                    {
-                        rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 3.0f);
-                    }
-                }
 
+        private void Update()
+        {
+            
+            if(bypass)
+            {
+                bypass = false;
+                Hit();
                 Invoke("DestroyMe", 2f);
             }
         }
+        void OnTriggerEnter(Collider hittingCollider)
+        {
+            Debug.Log(hittingCollider.transform.tag);
+            if(hittingCollider.CompareTag("PlayerSword"))
+            {
+                Hit();
+                explosionPos = hittingCollider.transform.position;
+                Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+                foreach (Collider hit in colliders)
+                {
+                    if (hit.CompareTag("EnemyChunk"))
+                    {
+                        Rigidbody rb = hit.GetComponent<Rigidbody>();
+                        if (rb != null)
+                        {
+                            rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 3.0f);
+                        }
+                    }
+
+                    Invoke("DestroyMe", 2f);
+                }
+            }
+        }
+
+        void Hit()
+        {
+                replacedMesh = Instantiate(brokenMesh, transform.position, transform.rotation);
+                this.GetComponent<BoxCollider>().enabled = false;
+                this.GetComponent<MeshRenderer>().enabled = false;
+                spawnEngine.registerKill();
+                //Gain Score Code Here
+        }
+
+        void DestroyMe()
+        {
+            spawnEngine.TestSpawn();
+            Destroy(replacedMesh);
+            Destroy(gameObject);
+        }
     }
 
-    void Hit()
-    {
-            replacedMesh = Instantiate(brokenMesh, transform.position, transform.rotation);
-            this.GetComponent<BoxCollider>().enabled = false;
-            this.GetComponent<MeshRenderer>().enabled = false;
-            //Gain Score Code Here
-    }
-
-    void DestroyMe()
-    {
-        spawnEngine.TestSpawn();
-        Destroy(replacedMesh);
-        Destroy(gameObject);
-    }
 }
