e2f83408529c7e5a00dfd0197064c6aa09923592
Ivanchia0909 <48830330+Ivanchia0909@users.noreply.github.com>
2019-11-21 09:04:34 +0800

    SpaceEnemyScript
    enemy chase script

---
+++ Assets/SpaceEscape/Scripts/Chase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public Transform player;
+    static Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < 30)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("Idel", false);
+            if (direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("Run", true);
+                anim.SetBool("Attack", false);
+            }
+            else
+            {
+                anim.SetBool("Attack", true);
+                anim.SetBool("Run", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Run", false);
+            anim.SetBool("Attack", false);
+        }
+        
+    }
+}
