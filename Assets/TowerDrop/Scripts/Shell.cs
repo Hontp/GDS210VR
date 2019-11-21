3e5a05221c599f8e33a08a2494b7113200dbb6f8
Hontp <puke_475@hotmail.com>
2019-11-21 11:34:13 +0800

    Tower Drop
    *Tank shell

---
+++ Assets/TowerDrop/Scripts/Shell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float shellSpeed = 5;
+
+
+    public void SetTarget( Transform targetTransform)
+    {
+        target = targetTransform;
+    }
+
+    public void SetShellSpeed( float speed)
+    {
+        shellSpeed = speed;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target.position);
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, shellSpeed * Time.deltaTime);
+        }
+    }
+}
