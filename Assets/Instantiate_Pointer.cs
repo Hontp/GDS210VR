b3ff5ff6d82cb17aec4ce85c02c1e3386dd08a04
IAmKoal <51438218+IAmKoal@users.noreply.github.com>
2019-12-03 19:51:37 +0800

    Pointer Stuff

---
+++ Assets/Instantiate_Pointer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SamuraiCutter;
+using Valve.VR.InteractionSystem;
+
+public class Instantiate_Pointer : MonoBehaviour
+{
+    public GameObject pointer;
+    public GameManager gm;
+    GameObject reference_pointer;
+    bool spawned_pointer;
+    public Hand hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.dead == true)
+        {
+            if (spawned_pointer == false)
+            {
+                reference_pointer=Instantiate(pointer);
+                spawned_pointer = true;
+                reference_pointer.transform.position = hand.gameObject.transform.position;
+                reference_pointer.transform.rotation = hand.gameObject.transform.rotation;
+                hand.AttachObject(reference_pointer, hand.GetBestGrabbingType(), Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.TurnOffGravity | Hand.AttachmentFlags.DetachOthers);
+            }
+
+        }
+        else
+        {
+            if (spawned_pointer == true)
+            {
+                hand.DetachObject(reference_pointer);
+                Destroy(reference_pointer);
+            }
+        }
+    }
+}
