3a881e151ee92d7be6774f73533fef208f19f6d8
SAE Laptop <>
2019-12-03 16:08:57 +0800

    fixed errprs made game cycle

--- Assets/MiniGames/SpaceEscape/Scripts/EnemySpawner.cs
+++ Assets/MiniGames/SpaceEscape/Scripts/EnemySpawner.cs
@@ -24,7 +24,7 @@ namespace MemeMachine
             SpawnTime = time;
             spawnLimiter = limit;
             spawnDecrease = decrease;
-            difficulty = dif
+            difficulty = dif;
         }
 
         [SerializeField]
