4c6992bbf88a0eacdd48a805e75e332a815de325
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2019-12-05 13:58:23 +0800

    added two throwabels

--- Assets/MiniGames/TowerDrop/Scripts/despawn_thowable.cs
+++ Assets/MiniGames/TowerDrop/Scripts/despawn_thowable.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class despawn_thowable : MonoBehaviour
 {
+    public bool explode;
+    public GameObject explosion;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
+        if (explode == true)
+        {
+            GameObject x;
+            x= Instantiate(explosion, transform);
+            x.transform.parent = null;
+        }
         Destroy(gameObject, 10);
     }
 }
