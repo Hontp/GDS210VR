1c34a35f71b3961232d7161206a823e157671c1a
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2019-12-11 16:46:55 +0800

    test sounds

---
+++ Assets/explosion_sound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDrop {
+    public class explosion_sound : MonoBehaviour
+    {
+        public TowerDropSoundController tsc = GameObject.Find("SoundManager").GetComponent<TowerDropSoundController>();
+        public Rigidbody rb;
+        bool soundPlayed;
+        // Start is called before the first frame update
+        void Start()
+        {
+            soundPlayed = false;
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rb.velocity.magnitude > 1 && soundPlayed == false)
+            {
+                soundPlayed = true;
+                tsc.playSFX(tsc.GetComponent<AudioSource>(), "0017_explo_bomb_04_PremiumBeat");
+            }
+        }
+    }
+}
