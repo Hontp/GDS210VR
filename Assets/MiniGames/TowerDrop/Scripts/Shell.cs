0b21958565ebaf93d1cfa9de84ec77ff94b67765
Hontp <puke_475@hotmail.com>
2019-12-11 22:32:09 +0800

    TowerDrop
    * Added Tank Laser
    * Added Vtol Laser
    * Added Shell Explosion

--- Assets/MiniGames/TowerDrop/Scripts/Shell.cs
+++ Assets/MiniGames/TowerDrop/Scripts/Shell.cs
@@ -9,9 +9,14 @@ namespace TowerDrop
         public float MaxDamage = 5.5f;
         public float MaxLifeTime = 5.5f;
 
+
+        public TowerDropSoundController sfxController;
+        private AudioSource sfxSource;
+
         private void Start()
-        {
+        {            
             Destroy(gameObject, MaxLifeTime);
+            sfxSource = GetComponent<AudioSource>();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -19,10 +24,14 @@ namespace TowerDrop
             if (other.tag == "Player")
             {
                 other.gameObject.GetComponent<PlayerHealth>().hit = true;
+
+                if (sfxController != null)
+                    sfxController.playSFX(sfxSource, "0017_explo_bomb_02");
+
                 Destroy(gameObject);
             }
 
             Destroy(gameObject, MaxLifeTime);
-        }
+        }   
     }
 }
