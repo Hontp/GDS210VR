49a6d9d928b171f07cc5cd9a7c72fa450e550d2d
IAmKoal <51438218+IAmKoal@users.noreply.github.com>
2019-12-11 09:13:55 +0800

    Working on a little something
    Enter at your own peril, past the bolted door
    Where impossible things may happen
    That the world has never seen before!
    
    In Josh's Laboratory
    Lives the smartest Person you've ever seen!
    But Unity blows their experiments
    To smithereens
    There is gloom and doom
    While things go "BOOM"...
    In Josh's La-a-a-a-ab!

---
+++ Assets/MiniGames/SamuraiCutter/Player/ChangeBladeColour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBladeColour : MonoBehaviour
+{
+    public Material blade;
+    public Vector4 blue, red, green, purple, yellow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blue = new Vector4(0, 150, 190, 1.5f);
+        red = new Vector4(140, 0, 0, 1.5f);
+        green = new Vector4(0, 70, 0, 1.5f);
+        purple = new Vector4(50, 0, 255, 1.5f);
+        yellow = new Vector4(150, 150, 20, 1.5f);
+
+        blade.SetColor("_Emission", blue);
+    }
+
+    public void MakeBlue()
+    {
+        blade.SetColor("_Emission", blue);
+    }
+
+    public void MakePurple()
+    {
+        blade.SetColor("_Emission", purple);
+    }
+
+    public void MakeRed()
+    {
+        blade.SetColor("_Emission", red);
+    }
+
+    public void MakeYellow()
+    {
+        blade.SetColor("_Emission", yellow);
+    }
+
+    public void MakeGreen()
+    {
+        blade.SetColor("_Emission", green);
+    }
+}
